1c360235132476de7fde7818ca8c580694b9cb0f
Tapio-adro <terentuk4321@gmail.com>
2022-09-18 13:44:49 +0300

    implemeted core function - search with autocomplete

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,627 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AutocompleteSearch :cities-array="dataArray"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AutocompleteSearch from './components/AutocompleteSearch.vue'
+// import cities from 'cities.json';
+let cities = [
+    {
+        "country": "AD",
+        "name": "Sant Julià de Lòria",
+        "lat": "42.46372",
+        "lng": "1.49129"
+    },
+    {
+        "country": "AD",
+        "name": "Pas de la Casa",
+        "lat": "42.54277",
+        "lng": "1.73361"
+    },
+    {
+        "country": "AD",
+        "name": "Ordino",
+        "lat": "42.55623",
+        "lng": "1.53319"
+    },
+    {
+        "country": "AD",
+        "name": "les Escaldes",
+        "lat": "42.50729",
+        "lng": "1.53414"
+    },
+    {
+        "country": "AD",
+        "name": "la Massana",
+        "lat": "42.54499",
+        "lng": "1.51483"
+    },
+    {
+        "country": "AD",
+        "name": "Encamp",
+        "lat": "42.53474",
+        "lng": "1.58014"
+    },
+    {
+        "country": "AD",
+        "name": "Canillo",
+        "lat": "42.5676",
+        "lng": "1.59756"
+    },
+    {
+        "country": "AD",
+        "name": "Arinsal",
+        "lat": "42.57205",
+        "lng": "1.48453"
+    },
+    {
+        "country": "AD",
+        "name": "Andorra la Vella",
+        "lat": "42.50779",
+        "lng": "1.52109"
+    },
+    {
+        "country": "AE",
+        "name": "Umm Al Quwain City",
+        "lat": "25.56473",
+        "lng": "55.55517"
+    },
+    {
+        "country": "AE",
+        "name": "Ras Al Khaimah City",
+        "lat": "25.78953",
+        "lng": "55.9432"
+    },
+    {
+        "country": "AE",
+        "name": "Muzayri‘",
+        "lat": "23.14355",
+        "lng": "53.7881"
+    },
+    {
+        "country": "AE",
+        "name": "Murbaḩ",
+        "lat": "25.27623",
+        "lng": "56.36256"
+    },
+    {
+        "country": "AE",
+        "name": "Maşfūţ",
+        "lat": "24.81089",
+        "lng": "56.10657"
+    },
+    {
+        "country": "AE",
+        "name": "Zayed City",
+        "lat": "23.65416",
+        "lng": "53.70522"
+    },
+    {
+        "country": "AE",
+        "name": "Khawr Fakkān",
+        "lat": "25.33132",
+        "lng": "56.34199"
+    },
+    {
+        "country": "AE",
+        "name": "Dubai",
+        "lat": "25.07725",
+        "lng": "55.30927"
+    },
+    {
+        "country": "AE",
+        "name": "Dibba Al-Fujairah",
+        "lat": "25.59246",
+        "lng": "56.26176"
+    },
+    {
+        "country": "AE",
+        "name": "Dibba Al-Hisn",
+        "lat": "25.61955",
+        "lng": "56.27291"
+    },
+    {
+        "country": "AE",
+        "name": "Sharjah",
+        "lat": "25.33737",
+        "lng": "55.41206"
+    },
+    {
+        "country": "AE",
+        "name": "Ar Ruways",
+        "lat": "24.11028",
+        "lng": "52.73056"
+    },
+    {
+        "country": "AE",
+        "name": "Al Manāmah",
+        "lat": "25.3299",
+        "lng": "56.02188"
+    },
+    {
+        "country": "AE",
+        "name": "Al Fujairah City",
+        "lat": "25.11641",
+        "lng": "56.34141"
+    },
+    {
+        "country": "AE",
+        "name": "Al Ain City",
+        "lat": "24.19167",
+        "lng": "55.76056"
+    },
+    {
+        "country": "AE",
+        "name": "Ajman City",
+        "lat": "25.40177",
+        "lng": "55.47878"
+    },
+    {
+        "country": "AE",
+        "name": "Adh Dhayd",
+        "lat": "25.28812",
+        "lng": "55.88157"
+    },
+    {
+        "country": "AE",
+        "name": "Abu Dhabi",
+        "lat": "24.45118",
+        "lng": "54.39696"
+    },
+    {
+        "country": "AE",
+        "name": "Khalifah A City",
+        "lat": "24.42588",
+        "lng": "54.605"
+    },
+    {
+        "country": "AE",
+        "name": "Bani Yas City",
+        "lat": "24.30978",
+        "lng": "54.62944"
+    },
+    {
+        "country": "AE",
+        "name": "Musaffah",
+        "lat": "24.35893",
+        "lng": "54.48267"
+    },
+    {
+        "country": "AE",
+        "name": "Al Shamkhah City",
+        "lat": "24.39268",
+        "lng": "54.70779"
+    },
+    {
+        "country": "AE",
+        "name": "Reef Al Fujairah City",
+        "lat": "25.14479",
+        "lng": "56.24764"
+    },
+    {
+        "country": "AE",
+        "name": "Al Wiqan",
+        "lat": "23.75355",
+        "lng": "55.32739"
+    },
+    {
+        "country": "AF",
+        "name": "Zōr Kōṯ",
+        "lat": "33.54149",
+        "lng": "69.73446"
+    },
+    {
+        "country": "AF",
+        "name": "Wulêswālī Bihsūd",
+        "lat": "34.3436",
+        "lng": "67.90567"
+    },
+    {
+        "country": "AF",
+        "name": "Kuhsān",
+        "lat": "34.65389",
+        "lng": "61.19778"
+    },
+    {
+        "country": "AF",
+        "name": "Lāsh",
+        "lat": "35.3782",
+        "lng": "64.77457"
+    },
+    {
+        "country": "AF",
+        "name": "Tukzār",
+        "lat": "35.94831",
+        "lng": "66.42132"
+    },
+    {
+        "country": "AF",
+        "name": "Bati",
+        "lat": "34.24348",
+        "lng": "70.72855"
+    },
+    {
+        "country": "AF",
+        "name": "Mīray",
+        "lat": "33.32462",
+        "lng": "68.44068"
+    },
+    {
+        "country": "AF",
+        "name": "Āq Kupruk",
+        "lat": "36.08352",
+        "lng": "66.84029"
+    },
+    {
+        "country": "AF",
+        "name": "Zurmat",
+        "lat": "33.43778",
+        "lng": "69.02774"
+    },
+    {
+        "country": "AF",
+        "name": "Zaybāk",
+        "lat": "36.52947",
+        "lng": "71.3441"
+    },
+    {
+        "country": "AF",
+        "name": "Zīārat-e Shāh Maqşūd",
+        "lat": "31.9848",
+        "lng": "65.4736"
+    },
+    {
+        "country": "AF",
+        "name": "Zindah Jān",
+        "lat": "34.34264",
+        "lng": "61.74675"
+    },
+    {
+        "country": "AF",
+        "name": "Zarghūn Shahr",
+        "lat": "32.84734",
+        "lng": "68.44573"
+    },
+    {
+        "country": "AF",
+        "name": "Zaṟah Sharan",
+        "lat": "33.14641",
+        "lng": "68.79213"
+    },
+    {
+        "country": "AF",
+        "name": "Zaranj",
+        "lat": "30.95962",
+        "lng": "61.86037"
+    },
+    {
+        "country": "AF",
+        "name": "Zamtō Kêlay",
+        "lat": "32.3726",
+        "lng": "66.17708"
+    },
+    {
+        "country": "AF",
+        "name": "Yangī Qal‘ah",
+        "lat": "37.46572",
+        "lng": "69.61131"
+    },
+    {
+        "country": "AF",
+        "name": "Bāzār-e Yakāwlang",
+        "lat": "34.73525",
+        "lng": "66.97379"
+    },
+    {
+        "country": "AF",
+        "name": "Yaḩyá Khēl",
+        "lat": "32.93742",
+        "lng": "68.64622"
+    },
+    {
+        "country": "AF",
+        "name": "Wāshēr",
+        "lat": "32.25122",
+        "lng": "63.85553"
+    },
+    {
+        "country": "AF",
+        "name": "Tōrmay",
+        "lat": "33.68847",
+        "lng": "68.40205"
+    },
+    {
+        "country": "AF",
+        "name": "Tūlak",
+        "lat": "33.97509",
+        "lng": "63.72868"
+    },
+    {
+        "country": "AF",
+        "name": "Tītān",
+        "lat": "33.69032",
+        "lng": "63.86361"
+    },
+    {
+        "country": "AF",
+        "name": "Tīr Pul",
+        "lat": "34.59431",
+        "lng": "61.26895"
+    },
+    {
+        "country": "AF",
+        "name": "Taywarah",
+        "lat": "33.52118",
+        "lng": "64.42116"
+    },
+    {
+        "country": "AF",
+        "name": "Bāzār-e Tashkān",
+        "lat": "36.88168",
+        "lng": "70.27674"
+    },
+    {
+        "country": "AF",
+        "name": "Tarinkot",
+        "lat": "32.62998",
+        "lng": "65.87806"
+    },
+    {
+        "country": "AF",
+        "name": "Taloqan",
+        "lat": "36.73605",
+        "lng": "69.53451"
+    },
+    {
+        "country": "AF",
+        "name": "Tagāw-Bāy",
+        "lat": "35.69941",
+        "lng": "66.06164"
+    },
+    {
+        "country": "AF",
+        "name": "Tagāb",
+        "lat": "34.85501",
+        "lng": "69.64917"
+    },
+    {
+        "country": "AF",
+        "name": "Markaz-e Ḩukūmat-e Sulţān-e Bakwāh",
+        "lat": "32.24139",
+        "lng": "62.94936"
+    },
+    {
+        "country": "AF",
+        "name": "Spīn Bōldak",
+        "lat": "31.00575",
+        "lng": "66.40001"
+    },
+    {
+        "country": "AF",
+        "name": "Spērah",
+        "lat": "33.20204",
+        "lng": "69.5152"
+    },
+    {
+        "country": "AF",
+        "name": "Sōzmah Qal‘ah",
+        "lat": "36.09916",
+        "lng": "66.20823"
+    },
+    {
+        "country": "AF",
+        "name": "Siyāhgird",
+        "lat": "35.00553",
+        "lng": "68.85578"
+    },
+    {
+        "country": "AF",
+        "name": "S̲h̲ēwah",
+        "lat": "34.57169",
+        "lng": "70.58859"
+    },
+    {
+        "country": "AF",
+        "name": "Shīnḏanḏ",
+        "lat": "33.30294",
+        "lng": "62.1474"
+    },
+    {
+        "country": "AF",
+        "name": "Shaykh Amīr Kêlay",
+        "lat": "33.28744",
+        "lng": "69.91283"
+    },
+    {
+        "country": "AF",
+        "name": "Qāshqāl",
+        "lat": "35.03975",
+        "lng": "69.00685"
+    },
+    {
+        "country": "AF",
+        "name": "Shibirghān",
+        "lat": "36.66757",
+        "lng": "65.7529"
+    },
+    {
+        "country": "AF",
+        "name": "Shwāk",
+        "lat": "33.42386",
+        "lng": "69.37684"
+    },
+    {
+        "country": "AF",
+        "name": "Shahr-e Şafā",
+        "lat": "31.80347",
+        "lng": "66.32376"
+    },
+    {
+        "country": "AF",
+        "name": "Shahrān",
+        "lat": "36.97984",
+        "lng": "70.73928"
+    },
+    {
+        "country": "AF",
+        "name": "Shahrak",
+        "lat": "34.10737",
+        "lng": "64.3052"
+    },
+    {
+        "country": "AF",
+        "name": "‘Alāqahdārī Shāh Jōy",
+        "lat": "32.52154",
+        "lng": "67.41315"
+    },
+    {
+        "country": "AF",
+        "name": "Wulêswālī Sayyid Karam",
+        "lat": "33.69056",
+        "lng": "69.36881"
+    },
+    {
+        "country": "AF",
+        "name": "Markaz-e Sayyidābād",
+        "lat": "34.00037",
+        "lng": "68.71346"
+    },
+    {
+        "country": "AF",
+        "name": "Şayād",
+        "lat": "36.13529",
+        "lng": "65.8297"
+    },
+    {
+        "country": "AF",
+        "name": "Sidqābād",
+        "lat": "35.02298",
+        "lng": "69.35112"
+    },
+    {
+        "country": "AF",
+        "name": "Sāyagaz",
+        "lat": "32.56521",
+        "lng": "67.03324"
+    },
+    {
+        "country": "AF",
+        "name": "Sar-e Tayghān",
+        "lat": "33.50998",
+        "lng": "65.67632"
+    },
+    {
+        "country": "AF",
+        "name": "Sarōbī",
+        "lat": "34.58962",
+        "lng": "69.76005"
+    },
+    {
+        "country": "AF",
+        "name": "Sar Kāṉī",
+        "lat": "34.79023",
+        "lng": "71.10962"
+    },
+    {
+        "country": "AF",
+        "name": "Sarfirāz Kalā",
+        "lat": "32.98333",
+        "lng": "67.96517"
+    },
+    {
+        "country": "AF",
+        "name": "Sar-e Pul",
+        "lat": "36.21544",
+        "lng": "65.93249"
+    },
+    {
+        "country": "AF",
+        "name": "Sar Chakān",
+        "lat": "35.70177",
+        "lng": "65.23055"
+    },
+    {
+        "country": "AF",
+        "name": "Sangīn",
+        "lat": "32.07275",
+        "lng": "64.8359"
+    },
+    {
+        "country": "AF",
+        "name": "Sang-e Māshah",
+        "lat": "33.1396",
+        "lng": "67.44046"
+    },
+    {
+        "country": "AF",
+        "name": "Sang-e Chārak",
+        "lat": "35.84972",
+        "lng": "66.43694"
+    },
+    {
+        "country": "AF",
+        "name": "Sang Atesh",
+        "lat": "35.248",
+        "lng": "63.0044"
+    },
+    {
+        "country": "AF",
+        "name": "Sangar Sarāy",
+        "lat": "34.40744",
+        "lng": "70.63937"
+    },
+    {
+        "country": "AF",
+        "name": "Aībak",
+        "lat": "36.26468",
+        "lng": "68.01551"
+    },
+    {
+        "country": "AF",
+        "name": "Rū-ye Sang",
+        "lat": "35.31999",
+        "lng": "67.63387"
+    },
+    {
+        "country": "AF",
+        "name": "Rūdbār",
+        "lat": "30.15",
+        "lng": "62.6"
+    },
+    {
+        "country": "AF",
+        "name": "Rustāq",
+        "lat": "37.12604",
+        "lng": "69.83045"
+    },
+    {
+        "country": "AF",
+        "name": "Rabāţ-e Sangī-ye Pā’īn",
+        "lat": "34.79951",
+        "lng": "62.13917"
+    },
+    {
+        "country": "AF",
+        "name": "Rāmak",
+        "lat": "33.52234",
+        "lng": "68.6264"
+    }
+];
 export default {
   name: 'App',
+  data() {
+    return {
+      dataArray: cities
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
   components: {
-    HelloWorld
+    AutocompleteSearch
   }
 }
 </script>
